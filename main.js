031b367a2cc5a182b5db82d55b8daeefc04ae8f9
jbnamuro <jbnamuro12@gmail.com>
2024-04-06 02:25:00 -0400

    added menu

--- main.js
+++ main.js
@@ -41,8 +41,82 @@ document.addEventListener("DOMContentLoaded", (event) => {
     },
   });
 
+  let clicked = false;
+  let barsTL = gsap.timeline({ paused: true });
+  barsTL.to("#bar1", {
+    rotate: 45,
+    y: 3,
+    duration: 0.25,
+    ease: "none",
+  });
+  barsTL.to(
+    "#bar2",
+    {
+      rotate: -45,
+      y: -3,
+      duration: 0.25,
+      ease: "none",
+    },
+    "<"
+  );
+
+  gsap.to(".nav-bars", {
+    scale: 1,
+    ease: "expo.out",
+    scrollTrigger: {
+      trigger: ".second-page",
+      start: "top 70%",
+      toggleActions: "play none none reverse",
+    },
+  });
+  document.querySelector(".nav-bars").addEventListener("click", () => {
+    if (!clicked) {
+      barsTL.play();
+      clicked = true;
+    } else {
+      barsTL.reverse();
+      clicked = false;
+    }
+  });
+
   document.getElementById("abtBtn").addEventListener("click", () => {
     lenis.scrollTo(".second-page");
+  });
+
+  document.querySelector(".nav-bars").addEventListener("click", () => {
+    if (clicked) {
+      gsap.to(".menu-items", {
+        clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
+        ease: "power2.out",
+      });
+      gsap.set(".clickOff", {
+        display: "block",
+      });
+    } else {
+      gsap.to(".menu-items", {
+        clipPath: "polygon(100% 0%, 100% 0, 100% 100%, 100% 100%)",
+        ease: "power2.out",
+        onComplete: () => {
+          gsap.set(".clickOff", {
+            display: "none",
+          });
+        },
+      });
+    }
+  });
+
+  document.querySelector(".clickOff").addEventListener("click", () => {
+    gsap.to(".menu-items", {
+      clipPath: "polygon(100% 0%, 100% 0, 100% 100%, 100% 100%)",
+      ease: "power2.out",
+      onComplete: () => {
+        gsap.set(".clickOff", {
+          display: "none",
+        });
+      },
+    });
+    barsTL.reverse();
+    clicked = false;
   });
 
   const lenis = new Lenis();
